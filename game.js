3489f0e03cbadf360081d69b5e7f6ba53b4ae35b
brunoyukkiwada <brunoyukkiwada@gmail.com>
2026-03-13 16:38:05 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,28 +1,61 @@
 const canvas = document.getElementById("game")
 const ctx = canvas.getContext("2d")
 
-let player = {
-x:400,
-y:300,
-speed:4
+function drawMap(){
+
+for(let y=0;y<map.length;y++){
+for(let x=0;x<map[y].length;x++){
+
+let tile = map[y][x]
+
+if(tile===0) ctx.fillStyle="#3c8f5a"
+if(tile===1) ctx.fillStyle="#1f5f3a"
+if(tile===2) ctx.fillStyle="#8b5a2b"
+if(tile===3) ctx.fillStyle="#c2b280"
+
+ctx.fillRect(
+x*tileSize,
+y*tileSize,
+tileSize,
+tileSize
+)
+
+}
 }
 
-let keys = {}
+}
 
-document.addEventListener("keydown",(e)=>{
-keys[e.key] = true
-})
+function drawPlayer(){
 
-document.addEventListener("keyup",(e)=>{
-keys[e.key] = false
-})
+ctx.fillStyle="blue"
+
+ctx.fillRect(
+player.x,
+player.y,
+player.size,
+player.size
+)
+
+}
+
+function drawEnemy(){
+
+ctx.fillStyle="red"
+
+ctx.fillRect(
+enemy.x,
+enemy.y,
+enemy.size,
+enemy.size
+)
+
+}
 
 function update(){
 
-if(keys["w"]) player.y -= player.speed
-if(keys["s"]) player.y += player.speed
-if(keys["a"]) player.x -= player.speed
-if(keys["d"]) player.x += player.speed
+movePlayer()
+
+document.getElementById("hp").innerText = player.hp
 
 }
 
@@ -30,8 +63,9 @@ function draw(){
 
 ctx.clearRect(0,0,800,600)
 
-ctx.fillStyle = "blue"
-ctx.fillRect(player.x,player.y,32,32)
+drawMap()
+drawPlayer()
+drawEnemy()
 
 }
 
