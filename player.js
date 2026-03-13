f4a6de271a39feca49bb1e161335eb2a6bcbc979
brunoyukkiwada <brunoyukkiwada@gmail.com>
2026-03-13 16:37:01 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,26 @@
+let player = {
+x:100,
+y:100,
+size:28,
+speed:3,
+hp:100
+}
 
+let keys = {}
+
+document.addEventListener("keydown", e => {
+keys[e.key] = true
+})
+
+document.addEventListener("keyup", e => {
+keys[e.key] = false
+})
+
+function movePlayer(){
+
+if(keys["w"]) player.y -= player.speed
+if(keys["s"]) player.y += player.speed
+if(keys["a"]) player.x -= player.speed
+if(keys["d"]) player.x += player.speed
+
+}
